6fa2592176135a6e9e660db7183bc83b5bffe453
John Codeos <johncodeos@gmail.com>
2022-06-29 16:23:55 +0300

    Added full-screen support for the WebView method

--- app/src/main/java/com/johncodeos/youtubeexample/WebViewActivity.kt
+++ app/src/main/java/com/johncodeos/youtubeexample/WebViewActivity.kt
@@ -1,19 +1,80 @@
 package com.johncodeos.youtubeexample
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
 
 @SuppressLint("SetJavaScriptEnabled")
 class WebViewActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
 
-        val webViewPlayerView = findViewById<WebView>(R.id.webview_player_view)
-        webViewPlayerView.settings.javaScriptEnabled = true
-        webViewPlayerView.loadUrl("https://www.youtube.com/embed/YE7VzlLtp-4")
+        webView = findViewById(R.id.webview_player_view)
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = CustomChromeClient() // Full-screen support
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.allowFileAccess = true
+        if (savedInstanceState == null) {
+            webView.loadUrl("https://www.youtube.com/embed/YE7VzlLtp-4")
+        }
+    }
+
+    private inner class CustomChromeClient : WebChromeClient() {
+        private var mCustomView: View? = null
+        private var mCustomViewCallback: CustomViewCallback? = null
+        private var mOriginalOrientation = 0
+        private var mOriginalSystemUiVisibility = 0
+        override fun getDefaultVideoPoster(): Bitmap? {
+            return if (mCustomView == null) {
+                null
+            } else BitmapFactory.decodeResource(applicationContext.resources, 2130837573)
+        }
+
+        override fun onHideCustomView() {
+            (window.decorView as FrameLayout).removeView(mCustomView)
+            mCustomView = null
+            window.decorView.systemUiVisibility = mOriginalSystemUiVisibility
+            requestedOrientation = mOriginalOrientation
+            mCustomViewCallback!!.onCustomViewHidden()
+            mCustomViewCallback = null
+        }
+
+        override fun onShowCustomView(
+            paramView: View?,
+            paramCustomViewCallback: CustomViewCallback?
+        ) {
+            if (mCustomView != null) {
+                onHideCustomView()
+                return
+            }
+            mCustomView = paramView
+            mOriginalSystemUiVisibility = window.decorView.systemUiVisibility
+            mOriginalOrientation = requestedOrientation
+            mCustomViewCallback = paramCustomViewCallback
+            (window.decorView as FrameLayout).addView(mCustomView, FrameLayout.LayoutParams(-1, -1))
+            window.decorView.systemUiVisibility = 3846 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        webView.saveState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        webView.restoreState(savedInstanceState)
     }
 }
